ea49ddd4fe87baf3f028cd2ff388cd5330622014
Adhil Bijukumar <75608493+Adhil-B@users.noreply.github.com>
2026-03-08 12:35:46 +0530

    Deloyment fix

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:8000/api/:path*',
+        },
+      ];
+    }
+    return [];
+  },
+};
+
+export default nextConfig;
